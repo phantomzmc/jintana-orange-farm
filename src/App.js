325738d4712425cf240225101135f34533c4351a
Thannathorn Yuwasin <thannathornyuw@gosoft.co.th>
2021-05-03 23:09:22 +0700

    update fuctions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Product from './component/product/product'
 import Hero from './component/hero/hero'
 import Header from './component/header/header'
 import Facts from './component/facts/facts'
-
+import './App.css'
 
 function App() {
   return (
@@ -13,27 +13,25 @@ function App() {
 
       <div className="content">
         <AbountMe />
-        <Facts />
-        <hr />
-        <h1 className="title">รูปสวน</h1>
-        <div class="grid grid-cols-3 gap-4">
-          {/* <img class="object-contain md:object-scale-down" src="https://scontent.fbkk12-4.fna.fbcdn.net/v/t1.6435-9/130575359_105664564746969_4795610443701436927_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeHuEtjFvdQ7QT7XatHGCYnLZ5JdiEpOnKBnkl2ISk6coAT2B2ghWTTvozcar1K-pq9qrpdGZqpugPThclI4PROn&_nc_ohc=iVcrH2TZe8sAX-2BPCL&_nc_ht=scontent.fbkk12-4.fna&oh=d4e7bb875abebc9a05a8fadd0b34a8c2&oe=609D7FC3" />
-          <img class="object-contain md:object-scale-down" src="https://scontent.fbkk12-4.fna.fbcdn.net/v/t1.6435-9/130575359_105664564746969_4795610443701436927_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeHuEtjFvdQ7QT7XatHGCYnLZ5JdiEpOnKBnkl2ISk6coAT2B2ghWTTvozcar1K-pq9qrpdGZqpugPThclI4PROn&_nc_ohc=iVcrH2TZe8sAX-2BPCL&_nc_ht=scontent.fbkk12-4.fna&oh=d4e7bb875abebc9a05a8fadd0b34a8c2&oe=609D7FC3" />
-          <img class="object-contain md:object-scale-down" src="https://scontent.fbkk12-4.fna.fbcdn.net/v/t1.6435-9/130575359_105664564746969_4795610443701436927_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeHuEtjFvdQ7QT7XatHGCYnLZ5JdiEpOnKBnkl2ISk6coAT2B2ghWTTvozcar1K-pq9qrpdGZqpugPThclI4PROn&_nc_ohc=iVcrH2TZe8sAX-2BPCL&_nc_ht=scontent.fbkk12-4.fna&oh=d4e7bb875abebc9a05a8fadd0b34a8c2&oe=609D7FC3" /> */}
-        </div>
-        <hr />
-        <h1 className="title">รูปสินค้า</h1>
+        <Facts />        
         <Product />
-        {/* <div class="grid grid-cols-4 gap-3">
-          
-        </div> */}
-        <hr />
-        <h1 className="title">แผนที่</h1>
-        <hr />
       </div>
-      <footer>
+      <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
 
-      </footer>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>Regna</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer>
     </div>
   );
 }
--- src/component/product/product.js
+++ src/component/product/product.js
@@ -1,90 +1,150 @@
+import '../commons/common.css'
+import './product.css'
+import AOS from 'aos';
+import Isotope from 'isotope-layout';
+import { select, on } from "../commons/common";
+
 function Product(params) {
-    return(
-        <div class="w-full mx-auto bg-white px-5 py-10 text-gray-600 mb-10">
-        <div class="text-center max-w-xl mx-auto">
-            <h1 class="text-5xl md:text-6xl font-bold mb-5">Pricing</h1>
-            <h3 class="text-xl font-medium mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit repellat dignissimos laboriosam odit accusamus porro</h3>
-        </div>
-        <div class="w-full md:flex mb-5">
-            <div class="w-full md:w-1/4 md:max-w-none bg-white px-8 md:px-10 py-8 md:py-10 mb-3 mx-auto md:my-6 rounded-md shadow-lg shadow-gray-600 md:flex md:flex-col">
-                <div class="w-full flex-grow">
-                    <h2 class="text-center font-bold uppercase mb-4">PERSONAL</h2>
-                    <h3 class="text-center font-bold text-4xl mb-2">$5<span class="text-lg">/mo</span></h3>
-                    <p class="text-center font-bold mb-5">
-                        {/* <a href="#" class="hover:underline hover:text-gray-700 transition-all transform hover:scale-110 inline-block">Read more<i class="mdi mdi-arrow-right-thick ml-1"></i></a> */}
-                    </p>
-                    <ul class="text-sm px-5 mb-8">
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Lorem ipsum</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Dolor sit amet</li>
-                    </ul>
+    /**
+   * Easy selector helper function
+   */
+
+    /**
+   * Porfolio isotope and filter
+   */
+
+    window.addEventListener('load', () => {
+        let portfolioContainer = select('.portfolio-container');
+        if (portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer, {
+                itemSelector: '.portfolio-item',
+                layoutMode: 'fitRows'
+            });
+
+            let portfolioFilters = select('#portfolio-flters li', true);
+
+            on('click', '#portfolio-flters li', function (e) {
+                e.preventDefault();
+                portfolioFilters.forEach(function (el) {
+                    el.classList.remove('filter-active');
+                });
+                this.classList.add('filter-active');
+
+                portfolioIsotope.arrange({
+                    filter: this.getAttribute('data-filter')
+                });
+                portfolioIsotope.on('arrangeComplete', function () {
+                    let refresh = new AOS();
+                    refresh.refresh();
+                });
+            }, true);
+        }
+
+    });
+
+    /**
+     * Initiate portfolio lightbox 
+     */
+    // const portfolioLightbox = new Glightbox({
+    //     selector: '.portfolio-lightbox'
+    // });
+
+    /**
+     * Portfolio details slider
+     */
+    // new Swiper('.portfolio-details-slider', {
+    //     speed: 400,
+    //     loop: true,
+    //     autoplay: {
+    //         delay: 5000,
+    //         disableOnInteraction: false
+    //     },
+    //     pagination: {
+    //         el: '.swiper-pagination',
+    //         type: 'bullets',
+    //         clickable: true
+    //     }
+    // });
+
+    /**
+     * Animation on scroll
+     */
+    window.addEventListener('load', () => {
+        let init = new AOS();
+        init.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        })
+    });
+    return (
+        <section id="portfolio" class="portfolio">
+            <div class="container" data-aos="fade-up">
+                <div class="section-header">
+                    <h3 class="section-title">สินค้า</h3>
+                    <p class="section-description">รายการสินค้า</p>
                 </div>
-                <div class="w-full">
-                    <button class="font-bold bg-gray-600 hover:bg-gray-700 text-white rounded-md px-10 py-2 transition-colors w-full">Buy Now</button>
+
+                <div class="row" data-aos="fade-up" data-aos-delay="100">
+                    <div class="col-lg-12 d-flex justify-content-center">
+                        <ul id="portfolio-flters">
+                            <li data-filter="*" class="filter-active">ทั้งหมด</li>
+                            {/* <li data-filter=".filter-4">เบอร์ 4</li> */}
+                            <li data-filter=".filter-5">เบอร์ 5</li>
+                            <li data-filter=".filter-6">เบอร์ 6</li>
+                            <li data-filter=".filter-all">คละเบอร์</li>
+                        </ul>
+                    </div>
                 </div>
+                <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-5">
+                        <img src={require(`../../images/no5_4.jpg`)} class="img-fluid" alt="" />
+                        <div class="portfolio-info">
+                            <h4>ส้มสายน้ำผึ้ง เบอร์ 5 40 ลูก</h4>
+                            <p>ราคา : 400 บาท</p>
+                            <a href="../../image/no5_4.jpg" data-gallery="portfolioGallery"
+                                class="portfolio-lightbox preview-link" title="Card 2"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-5">
+                        <img src="../../image/no5_5.jpg" class="img-fluid" alt="" />
+                        <div class="portfolio-info">
+                            <h4>ส้มสายน้ำผึ้ง เบอร์ 5 84 ลูก</h4>
+                            <p>ราคา : 750 บาท</p>
+                            <a href="../../image/no5_5.jpg" data-gallery="portfolioGallery"
+                                class="portfolio-lightbox preview-link" title="Web 2"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-6">
+                        <img src="../../image/no6_2.jpg" class="img-fluid" alt="" />
+                        <div class="portfolio-info">
+                            <h4>ส้มสายน้ำผึ้ง เบอร์ 6 30 ลูก</h4>
+                            <p>ราคา : 450 บาท</p>
+                            <a href="../../image/no6_2.jpgG" data-gallery="portfolioGallery"
+                                class="portfolio-lightbox preview-link" title="Card 1"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-6">
+                        <img src="../../image/no6_3.jpg" class="img-fluid" alt="" />
+                        <div class="portfolio-info">
+                            <h4>ส้มสายน้ำผึ้ง เบอร์ 6 72 ลูก</h4>
+                            <p>ราคา : 850 บาท</p>
+                            <a href="../../image/no6_3.jpg" data-gallery="portfolioGallery"
+                                class="portfolio-lightbox preview-link" title="Card 3"><i class="bx bx-plus"></i></a>
+                            <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
-            <div class="w-full md:w-1/4 md:max-w-none bg-white px-8 md:px-10 py-8 md:py-10 mb-3 mx-auto md:-mx-3 md:my-3 rounded-md shadow-lg shadow-gray-600 md:relative md:z-50 md:flex md:flex-col">
-                <div class="w-full flex-grow">
-                    <h2 class="text-center font-bold uppercase mb-4">TEAM</h2>
-                    <h3 class="text-center font-bold text-3xl md:text-4xl mb-2">$15<span class="text-lg">/mo</span></h3>
-                    <p class="text-center font-bold mb-5">
-                        {/* <a href="#" class="hover:underline hover:text-gray-700 transition-all transform hover:scale-110 inline-block">Read more<i class="mdi mdi-arrow-right-thick ml-1"></i></a> */}
-                    </p>
-                    <ul class="text-sm px-5 mb-8">
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Lorem ipsum</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Dolor sit amet</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Consectetur</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Adipisicing</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Elit repellat</li>
-                    </ul>
-                </div>
-                <div class="w-full">
-                    <button class="font-bold bg-gray-600 hover:bg-gray-700 text-white rounded-md px-10 py-2 transition-colors w-full">Buy Now</button>
-                </div>
-            </div>
-            <div class="w-full md:w-1/4 md:max-w-none bg-white px-8 md:px-10 py-8 md:py-10 mb-3 mx-auto md:-mx-3 md:mb-0 rounded-md shadow-lg shadow-gray-600 md:relative md:z-50 md:flex md:flex-col">
-                <div class="w-full flex-grow">
-                    <h2 class="text-center font-bold uppercase mb-4">PRO</h2>
-                    <h3 class="text-center font-bold text-4xl md:text-5xl mb-2">$35<span class="text-lg">/mo</span></h3>
-                    <p class="text-center font-bold mb-5">
-                        {/* <a href="#" class="hover:underline hover:text-gray-700 transition-all transform hover:scale-110 inline-block">Read more<i class="mdi mdi-arrow-right-thick ml-1"></i></a> */}
-                    </p>
-                    <ul class="text-sm px-5 mb-8">
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Lorem ipsum</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Dolor sit amet</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Consectetur</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Adipisicing</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Much more...</li>
-                    </ul>
-                </div>
-                <div class="w-full">
-                    <button class="font-bold bg-gray-600 hover:bg-gray-700 text-white rounded-md px-10 py-2 transition-colors w-full">Buy Now</button>
-                </div>
-            </div>
-            <div class="w-full md:w-1/4 md:max-w-none bg-white px-8 md:px-10 py-8 md:py-10 mb-3 mx-auto md:my-3 rounded-md shadow-lg shadow-gray-600 md:flex md:flex-col">
-                <div class="w-full flex-grow">
-                    <h2 class="text-center text-3xl font-bold uppercase mb-2">ENTERPRISE</h2>
-                    <p class="text-center font-bold mb-5">
-                        {/* <a href="#" class="hover:underline hover:text-gray-700 transition-all transform hover:scale-110 inline-block">Read more<i class="mdi mdi-arrow-right-thick ml-1"></i></a> */}
-                    </p>
-                    <ul class="text-sm px-5 mb-8">
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Lorem ipsum</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Consectetur</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Adipisicing</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Sed do eiusmod</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Aliquip ex</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Excepteur</li>
-                        <li class="leading-tight"><i class="mdi mdi-check-bold text-lg"></i> Dolor sit amet*</li>
-                    </ul>
-                </div>
-                <div class="w-full">
-                    <button class="font-bold bg-gray-600 hover:bg-gray-700 text-white rounded-md px-10 py-2 transition-colors w-full">Enquire Now</button>
-                </div>
-            </div>
-        </div>
-        <div class="text-center max-w-xl mx-auto">
-            <p class="text-xs leading-tight">*Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam eligendi officiis, impedit ducimus eaque a corporis, dolore quia officia quam tenetur suscipit dolores, quos, quaerat quo provident iusto. Eius, impedit!</p>
-        </div>
-    </div>
+        </section>
     )
 }
 
